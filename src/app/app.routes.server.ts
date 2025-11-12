d51042a44d9fb9fecad00dd29bc14a1a908382a8
mahmoudtawfek22 <momohoho2224@gmail.com>
2025-11-12 13:03:42 +0200

    improve routes.server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,29 +1,30 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
-import { People } from './components/people/people';
-import { Movies } from './components/movies/movies';
-import { Tv } from './components/tv/tv';
-import { MovieDetails } from './components/movie-details/movie-details';
-import { SeriesDetails } from './components/series-details/series-details';
+
 export const serverRoutes: ServerRoute[] = [
   {
     path: '',
     renderMode: RenderMode.Server,
   },
   {
-    path: 'movies/:id',
-
+    path: 'home',
     renderMode: RenderMode.Server,
   },
   {
-    path: 'home',
-
+    path: 'home/movie-details/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'home/series-details/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'movies/:id',
     renderMode: RenderMode.Server,
   },
   {
     path: 'movies/:id/movie-details/:id',
     renderMode: RenderMode.Server,
   },
-
   {
     path: 'people/:id',
     renderMode: RenderMode.Server,
@@ -37,18 +38,6 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server,
   },
   {
-    path: 'about',
-    renderMode: RenderMode.Prerender,
-  },
-  {
-    path: 'home/movie-details/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'home/series-details/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
     path: 'ai',
     renderMode: RenderMode.Server,
   },
@@ -57,7 +46,69 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server,
   },
   {
+    path: 'about',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
 ];
+
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '',
+//     renderMode: RenderMode.Server,
+//   },
+//   {
+//     path: 'movies/:id',
+
+//     renderMode: RenderMode.Server,
+//   },
+//   {
+//     path: 'home',
+
+//     renderMode: RenderMode.Server,
+//   },
+//   {
+//     path: 'movies/:id/movie-details/:id',
+//     renderMode: RenderMode.Server,
+//   },
+
+//   {
+//     path: 'people/:id',
+//     renderMode: RenderMode.Server,
+//   },
+//   {
+//     path: 'tv/:id',
+//     renderMode: RenderMode.Server,
+//   },
+//   {
+//     path: 'tv/:id/series-details/:id',
+//     renderMode: RenderMode.Server,
+//   },
+//   {
+//     path: 'about',
+//     renderMode: RenderMode.Prerender,
+//   },
+//   {
+//     path: 'home/movie-details/:id',
+//     renderMode: RenderMode.Server,
+//   },
+//   {
+//     path: 'home/series-details/:id',
+//     renderMode: RenderMode.Server,
+//   },
+//   {
+//     path: 'ai',
+//     renderMode: RenderMode.Server,
+//   },
+//   {
+//     path: 'ai/movie-details/:id',
+//     renderMode: RenderMode.Server,
+//   },
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Prerender,
+//   },
+// ];
